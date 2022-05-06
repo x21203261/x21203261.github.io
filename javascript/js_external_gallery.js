e11928c5e5b9e7a86c4680772a1c2eac123ad809
x21203261 <100739766+x21203261@users.noreply.github.com>
2022-05-06 22:29:50 +0100

    Add files via upload

--- javascript/js_external_gallery.js
+++ javascript/js_external_gallery.js
@@ -1,32 +1,42 @@
-// Gallery
+/* Gallery Page Functionality
+ * 
+ * This JavaScript file contains a number of functions:
+ * Functions 1 and 2 are called from clicks in the gallery html page
+ * Function 3 is then called from within functions 1 and 2
+ * i.e Function 1 and 2 are 2 separate ways of triggering function 3
+ *
+ * 1. changeSlide() - used show an image (via function 3) in slideshow from the arrow controls on the page
+ * 2. thumbSlide() - used to show an image (via function 3) in slideshow by selecting the related thumbnail
+ * 3. openSlide() - primary function used to open the image in the slideshow
+ * 
+ * */
+
+// Index to set that the first image be opened in slideshow by default when openSlide is called
 let slideIndex = 1;
-showSlides(slideIndex);
+openSlide (slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Called when forward/backward arrows clicked on the gallery page
+// Adds 1 (forwards) or adds -1 (backwards) to the index, then displays the slide with that index
+function changeSlide(n) {
+  openSlide(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// Called when a thumbnail is clicked on the gallery page
+// Index set equal to the index for the thumbnail (in the hallery html), then displays the slide with that index
+function thumbSlide(n) {
+  openSlide(slideIndex = n);
 }
 
-function showSlides(n) {
+// Function to display an image at set index in the slideshow container
+// Displays relevant element based on thumbnail or navigation arrow selection, and hides non-relevant elements
+function openSlide(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slides = document.getElementsByClassName("slideImage");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
